a0a11259bba7d5d4d3197f7ee94b83c4d63e88ce
mreorhan <videokod7@gmail.com>
2021-01-12 00:10:17 +0300

    Add Services Screen

---
+++ src/screens/messages/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export class ServicesScreen extends React.Component {
+  render() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Let's Get BiB'In</Text>
+      </View>
+    );
+  }
+}

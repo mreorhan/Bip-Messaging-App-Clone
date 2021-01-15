406d65d3fa6bd5f59c8445e2b2fd368557ac2bb5
mreorhan <videokod7@gmail.com>
2021-01-15 13:53:11 +0300

    Update Information Screen

--- src/components/base/Information.js
+++ src/components/base/Information.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import gStyles from '../../styles/gStyles';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
+import {Colors} from '../../styles/colors';
+import definitions from '../../styles/definitions';
+
+const style = StyleSheet.create({
+  informationText: {
+    backgroundColor: Colors.lightGrey,
+    color: Colors.grey,
+    borderRadius: definitions.button.radius,
+    paddingHorizontal: definitions.layout.gutters.sm,
+    paddingVertical: definitions.layout.gutters.xs,
+    fontFamily: 'moreo',
+    letterSpacing: 0.3,
+  },
+});
 
 export default ({text}) => {
   return (
     <View style={gStyles.flexCenter}>
-      <Text style={gStyles.informationText}>{text}</Text>
+      <Text style={style.informationText}>{text}</Text>
     </View>
   );
 };

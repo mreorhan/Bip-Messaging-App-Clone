3efaa0f050fb310f71ed02f46b340ee8555bde24
mreorhan <videokod7@gmail.com>
2021-01-15 20:53:12 +0300

    Update Press Component

--- src/components/base/Press.js
+++ src/components/base/Press.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import Ripple from 'react-native-material-ripple';
-import definitions from '../../styles/definitions';
+import {Colors} from '../../styles/colors';
+import {View, TouchableNativeFeedback} from 'react-native';
+import {useEffect} from 'react';
 
 export default (props) => {
+  let ripple = {
+    color: Colors.green,
+    val: false,
+    radius: 0,
+  };
+  useEffect(() => {});
+  if (props.circle) {
+    ripple = {
+      color: null,
+      val: false,
+      radius: 28,
+    };
+  }
   return (
-    <Ripple
-      rippleContainerBorderRadius={props.circle ? definitions.button.radius : 0}
-      {...props}>
-      {props.children}
-    </Ripple>
+    <TouchableNativeFeedback
+      onPress={props.onPress}
+      useForeground={false}
+      background={TouchableNativeFeedback.Ripple(
+        ripple.color,
+        ripple.val,
+        props.circle && ripple.radius,
+      )}
+      hitSlop={{top: 5, right: 5, bottom: 5, left: 5}}>
+      <View {...props}>{props.children}</View>
+    </TouchableNativeFeedback>
   );
 };
